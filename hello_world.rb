cae8be8c5043af0c7195e48470267ddfba325efb
Nicholas Martinez <nicholasmartinez@gmail.com>
2017-02-19 14:13:32 -0700

    test second file

--- hello_world.rb
+++ hello_world.rb
@@ -13,6 +13,6 @@ require_relative 'hamming' class HelloWorldTest < Minitest::Test
     assert_equal 'Hello, Bob!', HelloWorld.hello('Bob')
   end
 
-  def test_response
+  def test_response_2
   end
 end

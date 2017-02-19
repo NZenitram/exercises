7dc7c872c4d6931065f583ba850c7a14a19f4bf3
Nicholas Martinez <nicholasmartinez@gmail.com>
2017-02-19 14:35:00 -0700

    hit new method

--- hello_world.rb
+++ hello_world.rb
@@ -13,6 +13,6 @@ require_relative 'hamming' class HelloWorldTest < Minitest::Test
     assert_equal 'Hello, Bob!', HelloWorld.hello('Bob')
   end
 
-  def test_response_3
+  def test_response_method
   end
 end

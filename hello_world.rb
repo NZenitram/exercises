025c6e246a4714956506416319de97a09e3aa37b
Nicholas Martinez <nicholasmartinez@gmail.com>
2017-02-19 10:10:13 -0700

    updated hello_world.rb

---
+++ hello_world.rb
@@ -0,0 +1,15 @@
+gem 'minitest', '>= 5.0.0'
+require 'minitest/autorun'
+require_relative 'hamming' class HelloWorldTest < Minitest::Test
+  def test_no_name
+    assert_equal 'Hello, World!', HelloWorld.hello
+  end
+
+  def test_sample_name
+    assert_equal 'Hello, Alice!', HelloWorld.hello('Alice')
+  end
+
+  def test_other_sample_name
+    assert_equal 'Hello, Bob!', HelloWorld.hello('Bob')
+  end
+end

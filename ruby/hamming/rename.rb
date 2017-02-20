7b0b397243233578fd8697089a8ed2ba612c9559
Nicholas Martinez <nicholasmartinez@gmail.com>
2017-02-20 12:29:51 -0700

    update test

--- ruby/hamming/rename.rb
+++ ruby/hamming/rename.rb
@@ -3,9 +3,6 @@ gem 'minitest', '>= 5.0.0'
 require 'minitest/autorun'
 require_relative 'hamming'
 
-# Test data version:
-# deb225e Implement canonical dataset for scrabble-score problem (#255)
-
 class HammingTest < Minitest::Test
   def test_identical_strands
     skip
@@ -82,25 +79,3 @@ class HammingTest < Minitest::Test
     assert_raises(ArgumentError) { Hamming.compute('ATA', 'AGTG') }
   end
 end
-
-  # Problems in exercism evolve over time, as we find better ways to ask
-  # questions.
-  # The version number refers to the version of the problem you solved,
-  # not your solution.
-  #
-  # Define a constant named VERSION inside of the top level BookKeeping
-  # module.
-  #  In your file, it will look like this:
-  #
-  # module BookKeeping
-  #   VERSION = 1 # Where the version number matches the one in the test.
-  # end
-  #
-  # If you are curious, read more about constants on RubyDoc:
-  # http://ruby-doc.org/docs/ruby-doc-bundle/UsersGuide/rg/constants.html
-#
-#   def test_bookkeeping
-#     skip
-#     assert_equal 3, BookKeeping::VERSION
-#   end
-# end

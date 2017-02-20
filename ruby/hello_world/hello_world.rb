5365a50bcf6628d060529109ed9ba84c7abd412f
Nicholas Martinez <nicholasmartinez@gmail.com>
2017-02-20 09:37:54 -0700

    noah test

--- ruby/hello_world/hello_world.rb
+++ ruby/hello_world/hello_world.rb
@@ -13,6 +13,6 @@ require_relative 'hamming' class HelloWorldTest < Minitest::Test
     assert_equal 'Hello, Bob!', HelloWorld.hello('Bob')
   end
 
-  def not_again_pry 
+  def noah_test
   end
 end

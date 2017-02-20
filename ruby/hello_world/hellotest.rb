949a9cd1c0d9f0486002044abcd2036da5eece2c
Nicholas Martinez <nicholasmartinez@gmail.com>
2017-02-20 10:53:44 -0700

    pushing multiple exercises

--- ruby/hello_world/hellotest.rb
+++ ruby/hello_world/hellotest.rb
@@ -13,6 +13,6 @@ require_relative 'hamming' class HelloWorldTest < Minitest::Test
     assert_equal 'Hello, Bob!', HelloWorld.hello('Bob')
   end
 
-  def noah_test_folder_new_hash_name_passed
+  def noah_test_folder_new_hash_name_passed_all
   end
 end
